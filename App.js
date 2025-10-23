9b64a85bf1cc1fb42eea17e41d138b5360ec36f8
Gustavo <gustavo.blazius@escola.pr.gov.br>
2025-10-23 14:10:56 +0000

    aula 02

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {ScrollView} from 'react-native';
+import Titulo from "./Componentes/Titulo";
+import Saudacao from "./Componentes/Saudacao";
+
+export default function App(){
+  return(
+    <ScrollView style={{ padding:20}}>
+      <Titulo />
+      <Saudacao nome="Gustavo"/>
+
+    </ScrollView>
+  )
+}
